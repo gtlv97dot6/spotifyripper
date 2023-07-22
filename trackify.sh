9e6c355c0fc93a6375602c02d7623d139f5064a3
GTLv97.6 <gtlv97.6@gmail.com>
2023-07-22 20:39:11 +0200

    update trackify.sh script to work with the newer spotify html

--- trackify.sh
+++ trackify.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-string=$(echo "$1" | cut -d: -f3)
-string="http://open.spotify.com/track/$string"
+string=$1
 wget -q -O tmp.html "$string"
 
 # Echo out track number
-grep open.spotify.com/track tmp.html | cut -d\" -f4 | \
-  awk "{if (\$0 == \"$string\") {print NR}}"
+awk '/album:track/ {match($0, /"music:album:track" content="[0-9]+"/, arr); match(arr[0], /[0-9]+/, tn); print tn[0]}' tmp.html
 
 string=$(grep background: tmp.html | cut '-d/' -f 3,4,5 | cut '-d)' -f1)
 wget -q -O cover.jpg "$string"
