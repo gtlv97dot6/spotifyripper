9367de513059416d6b90f26b10c8e5d6f72533d3
GTLv97.6 <gtlv97.6@gmail.com>
2023-07-22 20:40:56 +0200

    disable cover download in trackify.sh

--- trackify.sh
+++ trackify.sh
@@ -6,7 +6,7 @@ wget -q -O tmp.html "$string"
 # Echo out track number
 awk '/album:track/ {match($0, /"music:album:track" content="[0-9]+"/, arr); match(arr[0], /[0-9]+/, tn); print tn[0]}' tmp.html
 
-string=$(grep background: tmp.html | cut '-d/' -f 3,4,5 | cut '-d)' -f1)
-wget -q -O cover.jpg "$string"
+# string=$(grep background: tmp.html | cut '-d/' -f 3,4,5 | cut '-d)' -f1)
+# wget -q -O cover.jpg "$string"
 
 rm -f tmp.html
